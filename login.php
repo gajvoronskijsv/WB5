db3da41d24dc702fba91c91bd85e4d588253d5a6
gajvoronskijsv <stas_gajvoronskij@mail.ru>
2020-04-23 09:53:02 +0300

    first commit

--- login.php
+++ login.php
@@ -27,9 +27,16 @@ if (!empty($_SESSION['login'])) {
 
 // В суперглобальном массиве $_SERVER PHP сохраняет некторые заголовки запроса HTTP
 // и другие сведения о клиненте и сервере, например метод текущего запроса $_SERVER['REQUEST_METHOD'].
-if ($_SERVER['REQUEST_METHOD'] == 'GET') {
+if ($_SERVER['REQUEST_METHOD'] == 'GET') 
+if (!empty($login_messages)) {
+  print('<div id="messages">');
+  // Выводим все сообщения.
+  foreach ($login_messages as $message) {
+    print($message);
+  }
+  print('</div>');
+}
 ?>
-
 <form action="" method="post">
   <input name="login" />
   <input name="pass" />
@@ -40,23 +47,29 @@ if ($_SERVER['REQUEST_METHOD'] == 'GET') {
 }
 // Иначе, если запрос был методом POST, т.е. нужно сделать авторизацию с записью логина в сессию.
 else {
-
+$login_messages[] = 'post works';
   // TODO: Проверть есть ли такой логин и пароль в базе данных.
   // Выдать сообщение об ошибках.
        
   $db = new PDO('mysql:host=localhost;dbname=u20296', u20296, 1377191);
     try {
+    	$login_messages[] = 'connected';
     	$db->query("SELECT * FROM `DBlab5` where login='".$_POST['login']."' AND pass='".$_POST['pass']."'");
 	}
 	catch(PDOException $e){
   	}
   if ($row) {
+  	$login_messages[] = 'rownotempty';
     // Если все ок, то авторизуем пользователя.
     $_SESSION['login'] = $_POST['login'];
     // Записываем ID пользователя.
-    $_SESSION['uid'] = 123;
+    $_SESSION['uid'] = $_POST['login'];
     // Делаем перенаправление.
     header('Location: index.php');
   }
-  else header('Location: login.php'); 
+  else 
+  {
+  	$login_messages[] = 'rowisempty';
+  	header('Location: login.php'); 
+  }
 }
